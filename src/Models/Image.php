838c24f3b03275bb7e9c12fa86789ebd5330bd4f
Eduardo Rengifo <eduardorengifo5@gmail.com>
2017-12-13 17:36:18 -0500

    Add Image model

---
+++ src/Models/Image.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BotNews\Models;
+
+/**
+ * Class Image
+ * @package BotNews\Models
+ */
+class Image extends Base {
+	/**
+	 * @var int
+	 */
+	private $width;
+
+	/**
+	 * @var int
+	 */
+	private $height;
+
+	/**
+	 * @return int
+	 */
+	public function getWidth() {
+		return $this->width;
+	}
+
+	/**
+	 * @param int $width
+	 */
+	public function setWidth( $width ) {
+		$this->width = $width;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getHeight() {
+		return $this->height;
+	}
+
+	/**
+	 * @param int $height
+	 */
+	public function setHeight( $height ) {
+		$this->height = $height;
+	}
+}
