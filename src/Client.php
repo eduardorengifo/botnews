e8aaff3cda68a31cf7e33b512db6b20fd08a8376
Eduardo Rengifo <eduardorengifo5@gmail.com>
2017-12-13 13:06:51 -0500

    Add Client class

---
+++ src/Client.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace BotNews;
+
+use BotNews\Helpers\Str;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Client extends \Goutte\Client {
+	/**
+	 * @var string
+	 */
+	protected $siteUrl;
+
+	/**
+	 * @return string
+	 */
+	public function getSiteUrl() {
+		return $this->siteUrl;
+	}
+
+	/**
+	 * @param string $siteUrl
+	 */
+	public function setSiteUrl( $siteUrl ) {
+		$this->siteUrl = $siteUrl;
+	}
+
+	/**
+	 * @param string $url
+	 *
+	 * @return array|mixed|object|string
+	 */
+	public function requestJsonLD( $url ) {
+		/** @var Crawler $crawler */
+		$crawler = $this->requestSanitize($url, 'GET');
+		return $this->getJsonLD($crawler);
+	}
+
+	/**
+	 * @param Crawler $crawler
+	 *
+	 * @return array|mixed|object|string
+	 */
+	public function getJsonLD($crawler) {
+		$jsonLD = $crawler->filter('script[type="application/ld+json"]')->first()->text();
+		$jsonLD = html_entity_decode( $jsonLD );
+		$jsonLD = json_decode ($jsonLD , TRUE);
+		return $jsonLD;
+	}
+
+	/**
+	 * @param string $url
+	 * @param string $method
+	 *
+	 * @return Crawler
+	 */
+	public function requestSanitize( $url, $method = 'GET' ) {
+		$url = Str::reduceDoubleSlashes($url);
+		return $this->request($method, $url);
+	}
+}
