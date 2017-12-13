aa91553cfd387c2147cbcb2f4a879d4d580eaa14
Eduardo Rengifo <eduardorengifo5@gmail.com>
2017-12-13 17:36:50 -0500

    Add Author model

---
+++ src/Models/Author.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BotNews\Models;
+
+/**
+ * Class Author
+ * @package BotNews\Models
+ */
+class Author extends Base {
+	/**
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * @var Image
+	 */
+	private $logo;
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setName( $name ) {
+		$this->name = $name;
+	}
+
+	/**
+	 * @return Image
+	 */
+	public function getLogo() {
+		return $this->logo;
+	}
+
+	/**
+	 * @param Image $logo
+	 */
+	public function setLogo( $logo ) {
+		$this->logo = $logo;
+	}
+}
