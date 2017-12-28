f43aadacb38bf0ca622d1bbf8353ecb09a94d1ba
Eduardo Rengifo <eduardorengifo5@gmail.com>
2017-12-28 02:58:37 -0500

    Add JsonLDTest

---
+++ tests/Helpers/JsonLDTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Helpers;
+
+use BotNews\Helpers\JsonLD;
+use BotNews\Models\Author;
+use BotNews\Models\Image;
+use BotNews\Models\Post;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class JsonLDTest
+ * @package Tests\Helpers
+ */
+class JsonLDTest extends TestCase
+{
+	/** @test */
+	public function testArticle()
+	{
+		$post = JsonLD::article([]);
+		$this->assertInstanceOf( Post::class, $post );
+	}
+
+	/** @test */
+	public function testOrganization()
+	{
+		$author = JsonLD::organization([]);
+		$this->assertInstanceOf( Author::class, $author );
+	}
+
+	/** @test */
+	public function testImageObject()
+	{
+		$image = JsonLD::imageObject([]);
+		$this->assertInstanceOf( Image::class, $image );
+	}
+}
